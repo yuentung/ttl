79b85786d5f2f2fd2d418aa49deee4d5af2c4589
Jerry Tung <jerry.tung@asizyo.com>
2021-11-10 15:17:50 +0800

    Fix: set page position to top after changing page

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Switch, Route, useLocation } from 'react-router-dom';
 import { ResetStyle, GlobalStyle } from './globalStyle';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import Home from '../Home';
 import Contact from '../Contact';
 
-const App = () => {
-  return (
-    <>
-      <ResetStyle />
-      <GlobalStyle />
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/ttl/contact" component={Contact} />
-          <Route path="/ttl" component={Home} />
-        </Switch>
-        <Footer />
-      </BrowserRouter>
-    </>
-  );
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'instant' });
+  }, [pathname]);
+
+  return <></>;
 };
+
+const App = () => (
+  <>
+    <ResetStyle />
+    <GlobalStyle />
+    <BrowserRouter>
+      <ScrollToTop />
+      <Navbar />
+      <Switch>
+        <Route path="/ttl/contact" component={Contact} />
+        <Route path="/ttl" component={Home} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  </>
+);
 
 export default App;
