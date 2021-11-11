d0a31a025532de58d49d1e74474cf02f4f708d47
Jerry Tung <jerry.tung@asizyo.com>
2021-11-11 14:31:55 +0800

    Refactor: Contact

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 import Banner from '../../components/Banner';
-import Content from './Content';
+import Form from './Form';
+import { forMobile, greaterThanMobile } from '../../theme/breakpoints';
 import data from '../../constants/data';
 
+const InnerWrapper = styled.div`
+  ${greaterThanMobile} {
+    max-width: 1440px;
+    padding: 120px 100px 160px;
+    margin: 0 auto;
+  }
+
+  ${forMobile} {
+    padding: 80px 32px;
+  }
+`;
+
+const Description = styled.p`
+  text-align: center;
+  font-size: 16px;
+  line-height: 30px;
+  letter-spacing: 2.5px;
+  font-weight: 400;
+  color: #505050;
+
+  ${greaterThanMobile} {
+    width: 360px;
+    margin: 0 auto 100px;
+  }
+
+  ${forMobile} {
+    margin-bottom: 80px;
+  }
+`;
+
 const Contact = () => {
-  const { banner, title, subTitle } = data.contact;
+  const { banner, title, subTitle, description } = data.contact;
 
   return (
     <>
@@ -13,7 +45,10 @@ const Contact = () => {
         title={title}
         subTitle={subTitle}
       />
-      <Content />
+      <InnerWrapper>
+        <Description>{description}</Description>
+        <Form />
+      </InnerWrapper>
     </>
   )
 };
